57182b62f02811b8f434b052a94e66ab215c425d
Flepsz <luisfelipelfsp3@gmail.com>
2023-09-04 16:50:26 -0300

    :sparkles: feat: dark mode added

--- hotpage/src/app/layout.tsx
+++ hotpage/src/app/layout.tsx
@@ -1,22 +1,29 @@
-import { Metadata } from 'next'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Providers from "./providers";
+import ThemeSwitcher from "./ThemeSwitcher";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Pandora',
-  description: 'Unlocking Infinite Possibilities',
-}
+  title: "Pandora",
+  description: "Unlocking Infinite Possibilities",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <ThemeSwitcher />
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
