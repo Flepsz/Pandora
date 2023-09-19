b7998f56ee3eb46a58603c724b8012e31648d790
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-09-18 22:52:39 -0300

    :sparkles: feat: Dynamic navbar links added

--- hotpage/src/constants/index.ts
+++ hotpage/src/constants/index.ts
@@ -1,36 +1,42 @@
+export const navLinks = [
+  { href: "/", key: "Home", text: "Home" },
+  { href: "/", key: "Products and Services", text: "Products and Services" },
+  { href: "/", key: "About Us", text: "About Us" },
+];
+
 export const footerLinks = [
-	{
-		title: "Pandora Bank",
-		links: ["About Us", "Careers", "Contact Us", "News", "Security"],
-	},
-	{
-		title: "Products and Services",
-		links: [
-			"Checking and Savings",
-			"Loans and Mortgages",
-			"Investments",
-			"Credit Cards",
-			"Online Banking",
-		],
-	},
-	{
-		title: "Customer Support",
-		links: ["FAQ", "Customer Service", "Branch Locator", "ATMs"],
-	},
-	{
-		title: "Legal",
-		links: ["Terms of Service", "Privacy Policy", "Cookie Policy"],
-	},
-	{
-		title: "Connect with Us",
-		links: ["Facebook", "Twitter", "LinkedIn", "Instagram"],
-	},
+  {
+    title: "Pandora Bank",
+    links: ["About Us", "Careers", "Contact Us", "News", "Security"],
+  },
+  {
+    title: "Products and Services",
+    links: [
+      "Checking and Savings",
+      "Loans and Mortgages",
+      "Investments",
+      "Credit Cards",
+      "Online Banking",
+    ],
+  },
+  {
+    title: "Customer Support",
+    links: ["FAQ", "Customer Service", "Branch Locator", "ATMs"],
+  },
+  {
+    title: "Legal",
+    links: ["Terms of Service", "Privacy Policy", "Cookie Policy"],
+  },
+  {
+    title: "Connect with Us",
+    links: ["Facebook", "Twitter", "LinkedIn", "Instagram"],
+  },
 ];
 
 export const underFooter = [
-	{
-		links: [
-			"Explore our Job Openings",
+  {
+    links: [
+      "Explore our Job Openings",
       "Dollar Exchange Rate",
       "Fees",
       "Security",
@@ -39,8 +45,8 @@ export const underFooter = [
       "Ethics Channel",
       "Information Security Policy",
       "Information Security Policy for Suppliers",
-		],
-		copyright: "© 2023 Pandora Bank S.A. EIN: 00.666.777/0001-01",
-		address: "Gaza Strip | Israel - Jah. Pod, 41 - Mossarr. Zip: P860",
-	},
+    ],
+    copyright: "© 2023 Pandora Bank S.A. EIN: 00.666.777/0001-01",
+    address: "Gaza Strip | Israel - Jah. Pod, 41 - Mossarr. Zip: P860",
+  },
 ];
