558f2e3bc76ff25aeca7688f4bc7dad0631dba69
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-09-19 11:30:29 -0300

    :lipstick: feat: More style and swiper in dev

--- hotpage/src/app/page.tsx
+++ hotpage/src/app/page.tsx
@@ -1,11 +1,15 @@
-import AppShow from "@/components/AppShow";
-import MainPage from "@/components/MainPage";
+import FeatureSwiper from "@/components/FeatureSwiper";
+import AppShow from "@/home/AppShow";
+import MainPage from "@/home/MainPage";
 
 export default function Home() {
 	return (
 		<main className="w-full min-h-screen pt-24">
 			<MainPage />
 			<AppShow />
+
+				<FeatureSwiper />
+
 		</main>
 	);
 }
