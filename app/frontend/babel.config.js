635cb2cb1cd9e68971f23d9fe86d9a42b7ceb325
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-10-06 10:02:32 -0300

    ci: Dependecies

--- app/frontend/babel.config.js
+++ app/frontend/babel.config.js
@@ -1,7 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			"nativewind/babel",
+			"transform-inline-environment-variables",
+			[
+				"@tamagui/babel-plugin",
+				{
+					components: ["tamagui"],
+					config: "./tamagui.config.ts",
+					logTimings: true,
+				},
+			],
+			"react-native-reanimated/plugin",
+		],
+	};
 };
