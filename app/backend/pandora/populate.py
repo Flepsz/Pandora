f176dbc12ff4ca1fb8266c91be5b65002979642a
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-11-13 11:30:51 -0300

    :sparkles: feat: populate initialized

--- app/backend/pandora/populate.py
+++ app/backend/pandora/populate.py
@@ -1,19 +1,19 @@
-from datetime import date, timedelta
-import datetime
-import json
-from datetime import date, timedelta
-import datetime
-import json
-from faker import Faker
-import requests
-import os
-import subprocess
+import requests, os, subprocess, multiprocessing
+from time import sleep
 
 BASE_URL = 'http://localhost:8000/api/v1/'
 
-populate_customers_url = os.path.join(BASE_URL, 'customers/')
-
-fake = Faker()
+user_create_url = os.path.join(BASE_URL, 'auth/users/')
+jwt_create_url = os.path.join(BASE_URL, 'auth/jwt/create/')
+natural_person_url = os.path.join(BASE_URL, 'customersnp/')
+legal_person_url = os.path.join(BASE_URL, 'customerslp/')
+contacts_url = os.path.join(BASE_URL, 'contacts/')
+address_url = os.path.join(BASE_URL, 'addresses/')
+account_url = os.path.join(BASE_URL, 'accounts/')
+card_url = os.path.join(BASE_URL, 'cards/')
+transaction_url = os.path.join(BASE_URL, 'transactions/')
+investment_url = os.path.join(BASE_URL, 'investments/')
+create_loan_url = os.path.join(BASE_URL, 'loans/')
 
 
 def data_base_creation():
@@ -43,185 +43,131 @@ def superuser_creation():
         print(e)
     except Exception as e:
         print(e)
-        
-
-class DateEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (date, datetime, timedelta)):
-            return obj.isoformat()
-        return super(DateEncoder, self).default(obj)
-    
-
-def date_to_iso_string(date_obj):
-    if isinstance(date_obj, (date, datetime, timedelta)):
-        return date_obj.isoformat()
-    return date_obj
-
-def populate_customers(num_customers=10):
-    for _ in range(num_customers):
-        birthdate = fake.date_of_birth()
-        birthdate_iso = date_to_iso_string(birthdate)
-
-        response = requests.post(populate_customers_url,
-            json={
-                "full_name": fake.name(),
-                "social_name": fake.name(),
-                "birthdate": birthdate_iso,
-                "username": fake.user_name(),
-                "password": fake.password(),
-            })
-
-        return response.json
 
 
-# def populate_customer_np(num_customers=10):
-#     for _ in range(num_customers):
-#         response = requests.post(
-#             json={
-#                 "full_name": fake.name(),
-#                 "social_name": fake.name(),
-#                 "birthdate": fake.date_of_birth(),
-#                 "username": fake.user_name(),
-#                 "password": fake.password(),
-#                 "cpf": fake.unique.random_int(min=10000000000, max=99999999999),
-#                 "rg": fake.unique.random_int(min=100000000, max=999999999),
-#             })
-#         return response.json()
+def user_create(first_name, last_name, register_number, password, picture='a'):
+    response = requests.post(user_create_url,
+                             json={
+                                 "first_name": first_name,
+                                 "last_name": last_name,
+                                 "register_number": register_number,
+                                 "picture": picture,
+                                 "password": password,
+                             })
+    return response.json()
 
 
-# def populate_customer_lp(num_customers=10):
-#     for _ in range(num_customers):
-#         response = requests.post(
-#             json={
-#                 "full_name": fake.name(),
-#                 "social_name": fake.name(),
-#                 "birthdate": fake.date_of_birth(),
-#                 "username": fake.user_name(),
-#                 "password": fake.password(),
-#                 "cnpj": fake.unique.random_int(
-#                     min=10000000000000, max=99999999999999),
-#                 "state_registration": fake.unique.random_int(
-#                     min=100000000, max=999999999),
-#                 "municipal_registration": fake.unique.random_int(
-#                     min=10000000000, max=99999999999),
-#             })
-#         return response.json()
+def create_headers(register_number, password):
+    response = requests.post(jwt_create_url,
+                             json={
+                                 'register_number': register_number,
+                                 'password': password
+                             })
+    access_token = response.json()['access']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    return headers
 
 
-# def populate_account(num_accounts=20):
-#     customers = Customer.objects.all()
-#     for _ in range(num_accounts):
-#         account = Account(
-#             acc_type=fake.random_element(elements=('SAVINGS', 'CHECKING')),
-#             limit=random.uniform(1000, 10000),
-#             active=fake.boolean(chance_of_getting_true=90),
-#         )
-#         account.save()
-#         account.customer.set(random.sample(
-#             list(customers), random.randint(1, 3)))
+def create_natural_person(headers, register_number, name, birth_date, rg, social_name):
+    response = requests.post(natural_person_url, headers=headers,
+                             json={
+                                 'customer': register_number,
+                                 'name': name,
+                                 'social_name': social_name,
+                                 'cpf': str(register_number),
+                                 'rg': rg,
+                                 'birth_date': birth_date
+                             })
+    return response.json()
 
 
-# def populate_address(num_addresses=30):
-#     for _ in range(num_addresses):
-#         address = Address(
-#             street=fake.street_address(),
-#             neighborhood=fake.city_suffix(),
-#             city=fake.city(),
-#             state=fake.state_abbr(),
-#             zip_code=fake.zipcode(),
-#         )
-#         address.save()
+def create_legal_person(headers, register_number, fantasy_name, establishment_date, municipal_registration, state_registration):
+    response = requests.post(legal_person_url, headers=headers,
+                             json={
+                                 'customer': register_number,
+                                 'fantasy_name': fantasy_name,
+                                 'establishment_date': establishment_date,
+                                 'cnpj': str(register_number),
+                                 'sr': state_registration,
+                                 'mr': municipal_registration,
+                             })
+    return response.json()
 
 
-# def populate_contact():
-#     customers = Customer.objects.all()
-#     for customer in customers:
-#         contact = Contact(
-#             idCustomer=customer,
-#             number=fake.phone_number(),
-#             email=fake.email(),
-#             observation=fake.text(max_nb_chars=200),
-#         )
-#         contact.save()
+def create_address(headers, register_number, street, number, neighborhood, city, state, cep):
+    response = requests.post(address_url, headers=headers, json={
+        'customer': register_number,
+        'street': street,
+        'number': number,
+        'neighborhood': neighborhood,
+        'city': city,
+        'state': state,
+        'zip_code': cep
+    })
+    return response.json()
 
 
-# def populate_card(num_cards=50):
-#     accounts = Account.objects.all()
-#     for _ in range(num_cards):
-#         card = Card(
-#             idAccount=random.choice(accounts),
-#             active=fake.boolean(chance_of_getting_true=90),
-#         )
-#         card.save()
+def create_contact(headers, register_number, number, email):
+    response = requests.post(address_url, headers=headers, json={
+        'customer': register_number,
+        'number': number,
+        'email': email,
+    })
+    return response.json()
 
 
-# def populate_transaction(num_transactions=100):
-#     cards = Card.objects.all()
-#     for _ in range(num_transactions):
-#         transaction = Transaction(
-#             idCard=random.choice(cards),
-#             date_time=fake.date_time_this_decade(),
-#             operation=fake.random_element(
-#                 elements=('Deposit', 'Withdrawal', 'Payment')),
-#             amount=random.uniform(10, 1000),
-#         )
-#         transaction.save()
+def create_account(headers, acc_type, register_number):
+    response = requests.post(account_url, headers=headers, json={
+        'customer': [register_number],
+        'acc_type': acc_type
+    })
+    return response.json()
 
 
-# def populate_investment(num_investments=10):
-#     accounts = Account.objects.all()
-#     for _ in range(num_investments):
-#         investment = Investment(
-#             idAccount=random.choice(accounts),
-#             inv_type=fake.random_element(
-#                 elements=('Stocks', 'Bonds', 'Mutual Funds', 'Real Estate')),
-#             amount=random.uniform(1000, 10000),
-#             management_fee=random.uniform(0.5, 3.0),
-#             term=timedelta(days=random.randint(365, 3650)),
-#             risk_rate=random.uniform(0.5, 10.0),
-#             profitability=random.uniform(2.0, 12.0),
-#         )
-#         investment.save()
-
-
-# def populate_loan(num_loans=5):
-#     accounts = Account.objects.all()
-#     for _ in range(num_loans):
-#         loan = Loan(
-#             idAccount=random.choice(accounts),
-#             request_date=fake.date_this_decade(),
-#             requested_amount=random.uniform(1000, 10000),
-#             interest_rate=random.uniform(3.0, 12.0),
-#             approved=fake.boolean(chance_of_getting_true=80),
-#             approval_date=fake.date_between(
-#                 start_date='-30d', end_date='today') if fake.boolean(chance_of_getting_true=80) else None,
-#             installment_number=random.randint(6, 36),
-#             observation=fake.text(max_nb_chars=200),
-#         )
-#         loan.save()
-
-
-# def populate_installment_loan():
-#     loans = Loan.objects.filter(approved=True)
-#     for loan in loans:
-#         for installment_number in range(1, loan.installment_number + 1):
-#             installment = InstallmentLoan(
-#                 idLoan=loan,
-#                 number=installment_number,
-#                 due_date=fake.date_between_dates(
-#                     loan.approval_date, loan.approval_date + timedelta(days=365)),
-#                 amount=loan.requested_amount / loan.installment_number,
-#             )
-#             installment.save()
+def create_card(headers, number_account):
+	response = requests.post(card_url, headers=headers, json={
+		'account': number_account
+	})
+	return response.json()
 
 
 def main():
-    data_base_creation(),
-    # superuser_creation()
-    # populate_customers()
-    # populate_customer_np()
-    # populate_customer_lp()
-    run_server()
+    data_base_creation()
+    server_process = multiprocessing.Process(target=run_server)
+    server_process.start()
+    sleep(1)
+
+    superuser_creation()
+    super_user_header = create_headers(123,'123')
+
+    # NATURAL PERSON REGISTRASTION
+    print(user_create(123456, "test@test"))
+    headers_1 = create_headers(123456, "test@test")
+    print(headers_1)
+    print(create_natural_person(headers_1, 123456, 'Luís', '2005-11-03', '222222', 'Felipe'))
+ 
+    print(user_create(1234567, "test@test"))
+    headers_2 = create_headers(1234567, "test@test")
+    print(create_natural_person(headers_2, 1234567, 'Luís', '2005-11-03', '222222', 'Felipe'))
+
+	# LEGAL PERSON REGISTRASTION
+    print(user_create(654321, "test@test"))
+    headers_3 = create_headers(654321, "test@test")
+    print(create_legal_person(headers_3, 654321, 'Fantasy', '2023-06-19', '1234', '4321', 'Cars'))
+ 
+    print(user_create(7654321, "test@test"))
+    headers_4 = create_headers(7654321, "test@test")
+    print(create_legal_person(headers_4, 7654321, 'Fantasy', '2023-06-19', '1234', '4321', 'Cars'))
+
+    # ADDRESS REGISTRASTION
+    print(create_address(headers_1, 123456, 'Rua Jones', '69', 'LeWhite Green', 'Varsóvia', 'Polônia', '12564789'))
+
+    # ACCOUNT REGISTRASTION
+    print(create_account(headers_1, 'Savings'))
+    print(create_account(headers_3, 'Current'))	
+
+    print(create_card(headers_1, 1111))
+
 
 
 if __name__ == '__main__':
