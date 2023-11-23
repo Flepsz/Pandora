595b01d6d1e3a9ed8c73bf84a676d84b59252abd
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-11-23 17:29:57 +0000

    :sparkles: feat: Connection with api on register

--- hotpage/src/components/Forms.tsx
+++ hotpage/src/components/Forms.tsx
@@ -3,28 +3,82 @@ import React, { useCallback, useState } from "react";
 import ButtonPW from "./ButtonPW";
 import Link from "next/link";
 import Input from "./Input";
+import axios from "axios"
 
 interface FormsI {
 	isRegister?: boolean;
 	isCustomerNP?: boolean;
 }
 
+export const axiosInstance = axios.create({
+	baseURL: "http://127.0.0.1:8056/api/v1/",
+});
+
 export default function Forms({ isRegister, isCustomerNP }: FormsI) {
+	const [registerNumber, setRegisterNumber] = useState("");
 	const [password, setPassword] = useState("");
 
 	const [name, setName] = useState("");
 	const [socialName, setSocialName] = useState("");
-	const [cpf, setCpf] = useState("");
 	const [rg, setRg] = useState("");
 	const [birthdate, setBirthdate] = useState("");
 
-	const [cnpj, setCnpj] = useState("");
 	const [fantasyName, setFantasyName] = useState("");
 	const [stateRegistration, setStateRegistration] = useState("");
 	const [municipalRegistration, setMunicipalRegistration] = useState("");
 	const [establishmentDate, setEstablishmentDate] = useState("");
 
-	const registerAccount = useCallback(async () => {}, [])
+	const registerAccount = useCallback(async () => {
+		try {
+			const createUser = await axiosInstance.post("auth/users/", {
+				register_number: registerNumber,
+				picture: "777",
+				password: password,
+			});
+			console.log(createUser);
+			if (createUser.status === 201) {
+				const jwtToken = await axiosInstance.post("auth/jwt/create/", {
+					register_number: registerNumber,
+					password: password,
+				});
+				console.log(jwtToken.data.access);
+				if (isCustomerNP) {
+					const createNP = await axiosInstance.post("customersnp/", {
+						customer: registerNumber,
+						name: name,
+						social_name: socialName,
+						cpf: registerNumber,
+						rg: rg,
+						birthdate: birthdate
+					},
+						{
+							headers: {
+								Authorization: `Bearer ${jwtToken.data.access}`,
+							},
+						});
+					console.log(createNP);
+				} else {
+					const createLP = await axiosInstance.post("customerslp/", {
+						customer: registerNumber,
+						fantasy_name: fantasyName,
+						cnpj: registerNumber,
+						sr: stateRegistration,
+						mr: municipalRegistration,
+						establishment_date: establishmentDate
+					},
+						{
+							headers: {
+								Authorization: `Bearer ${jwtToken.data.access}`,
+							},
+						});
+					console.log(createLP);
+					
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}, [registerNumber, password, isCustomerNP, name, socialName, rg, birthdate, fantasyName, stateRegistration, municipalRegistration, establishmentDate])
 
 	return (
 		<div className="h-full gradient-form bg-neutral-200 dark:bg-neutral-700">
@@ -56,8 +110,8 @@ export default function Forms({ isRegister, isCustomerNP }: FormsI) {
 												<Input
 													placeholder={isCustomerNP ? "CPF" : "CNPJ"}
 													label={isCustomerNP ? "CPF" : "CNPJ"}
-													onChange={isCustomerNP ? setCpf : setCnpj}
-													value={isCustomerNP ? cpf : cnpj}
+													onChange={isCustomerNP ? setRegisterNumber : setRegisterNumber}
+													value={isCustomerNP ? registerNumber : registerNumber}
 												/>
 
 												<Input
@@ -144,7 +198,7 @@ export default function Forms({ isRegister, isCustomerNP }: FormsI) {
 														? "Already have an account?"
 														: "Don't have an account?"}
 												</p>
-												<Link href={isRegister ? (isCustomerNP ? "/login/customernp" : "/login/customerlp") : (isCustomerNP ? "/register/customernp" : "/register/customerlp")}>
+												<Link onClick={registerAccount} href={isRegister ? (isCustomerNP ? "/login/customernp" : "/login/customerlp") : (isCustomerNP ? "/register/customernp" : "/register/customerlp")}>
 													<button
 														type="button"
 														className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
